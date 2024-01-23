ee2cdc24123c5aa8324d7e7f0669613fcea39d67
Ahmad Dzaky <ahmaddzakyjamaludin@mail.ugm.ac.id>
2024-01-23 21:56:00 +0700

    first commit

---
+++ 1-evenOrOdd.py
@@ -0,0 +1,5 @@
+# coding challenge 1/366
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/python
+
+def even_or_odd(number):
+  return 'Even' if number % 2 == 0 else 'Odd'
---
+++ 2-reversedSequence.py
@@ -0,0 +1,4 @@
+# coding challenge 2/366
+
+def reverse_seq(n):
+  return list(range(n, 0, -1))
