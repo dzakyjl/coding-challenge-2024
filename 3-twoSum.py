5026ee4f022478f3158a28ec60fe56cd3699cba9
Ahmad Dzaky <ahmaddzakyjamaludin@mail.ugm.ac.id>
2024-01-24 13:33:15 +0700

    add alternate solution

--- 3-twoSum.py
+++ 3-twoSum.py
@@ -10,3 +10,12 @@ class Solution(object):
         return [seen[complement], i]
       seen[num] = i
     return []
+  
+  # alternate solution (brute force)
+  
+class Solution(object):
+  def twoSum(self, nums, target):
+    for i in range(len(nums)):
+      for j in range(i + 1, len(nums)):
+        if nums[j] == target - nums[i]:
+          return [i, j]
